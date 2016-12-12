32e82f7e0de002f85a4dedc8f8a0f96dadc8414e
qoder <qoder@hrsoft.net>
2016-12-12 21:30:27 +0800

    删除了无用文件

--- src/components/plugins/loading/index.ts.tsx
+++ src/components/plugins/loading/index.ts.tsx
@@ -3,7 +3,7 @@
  * Created by qoder on 16/12/12.
  */
 declare const require: any;
-import *as React from 'react'
+import * as React from 'react'
 const s = require('./index.scss');
 
 const Loading = ()=> {
